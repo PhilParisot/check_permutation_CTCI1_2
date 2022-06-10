02a389ae19b58f0805b8dbff8b7c758b57c5dda3
Philippe Parisot <pparisot3@gmail.com>
2022-06-09 22:55:25 -0400

    sort check with len() check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", check_permutation_sort("abc", "cbaa"));
+}
+
+fn check_permutation_sort(string1: &str, string2: &str) -> bool {
+    if string1.len() != string2.len() {
+        return false;
+    }
+
+    let mut string1: Vec<_> = string1.chars().collect();
+    string1.sort();
+
+    let mut string2: Vec<_> = string2.chars().collect();
+    string2.sort();
+
+    string1 == string2
 }
